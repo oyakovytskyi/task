2671ef5ad961fe38e4abcdd5f6b2e410379e8a62
oyakovytskyi <oyakovytskyi.dev@gmail.com>
2024-09-26 05:55:31 +0200

    fix

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   app.enableCors({
     origin: 'https://task-zenbit-frontend.vercel.app',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
     credentials: true,
   });
 
